444341690ba79551f3eed400e9d9adf2712a4f26
nomanliaqat48 <noman.liaqat@invozone.dev>
2025-06-18 14:30:34 +0500

    Remove unused code

--- src/services/xmlBuilder.js
+++ src/services/xmlBuilder.js
@@ -13,24 +13,24 @@ function buildOrderXML(order) {
         sourceVendor: 'Shopify'
       },
       order: {
-        orderNumber: order.order_number,
+        orderNumber: order?.order_number,
         customer: {
           customerAcctId: '',
-          customerBusinessName: order.customer.company || '',
-          customerNameFirst: order.customer.first_name,
-          customerNameLast: order.customer.last_name,
-          customerPhone: order.customer.phone || '',
-          customerAddress: order.billing_address.address1,
-          customerCity: order.billing_address.city,
-          customerState: order.billing_address.province_code,
-          customerZip: order.billing_address.zip,
-          customerCountry: order.billing_address.country_code,
-          customerEmail: order.customer.email
+          customerBusinessName: order.customer?.company || '',
+          customerNameFirst: order.customer?.first_name,
+          customerNameLast: order.customer?.last_name,
+          customerPhone: order.customer?.phone || '',
+          customerAddress: order.billing_address?.address1,
+          customerCity: order.billing_address?.city,
+          customerState: order.billing_address?.province_code,
+          customerZip: order.billing_address?.zip,
+          customerCountry: order.billing_address?.country_code,
+          customerEmail: order.customer?.email
         },
         recipient: {
-          recipientFirstName: order.shipping_address.first_name,
-          recipientLastName: order.shipping_address.last_name,
-          recipientPhone: order.shipping_address.phone
+          recipientFirstName: order.shipping_address?.first_name,
+          recipientLastName: order.shipping_address?.last_name,
+          recipientPhone: order.shipping_address?.phone
         },
         card: {
           cardMessage: getCustomAttr(order, 'card_message') || '',
@@ -38,28 +38,28 @@ function buildOrderXML(order) {
         },
         delivery: {
           deliveryDate: getCustomAttr(order, 'delivery_date') || '',
-          deliveryBusinessName: order.shipping_address.company || '',
-          deliveryAddress1: order.shipping_address.address1,
-          deliveryAddress2: order.shipping_address.address2 || '',
-          deliveryCity: order.shipping_address.city,
-          deliveryState: order.shipping_address.province_code,
-          deliveryZipCode: order.shipping_address.zip,
-          deliveryInstructions: order.note || '',
-          deliveryCharge: order.total_shipping_price_set?.shop_money?.amount || '0.00',
+          deliveryBusinessName: order?.shipping_address?.company || '',
+          deliveryAddress1: order?.shipping_address?.address1,
+          deliveryAddress2: order?.shipping_address?.address2 || '',
+          deliveryCity: order?.shipping_address?.city,
+          deliveryState: order?.shipping_address?.province_code,
+          deliveryZipCode: order.shipping_address?.zip,
+          deliveryInstructions: order?.note || '',
+          deliveryCharge: order?.total_shipping_price_set?.shop_money?.amount || '0.00',
           wireout: 'N',
           wireoutServiceCharge: '0.00',
           pickup: 'N',
           orderTaxPercent: '0.00',
-          orderNotes: order.note || ''
+          orderNotes: order?.note || ''
         },
         product: {
-          productDetails: order.line_items.map(item => ({
-            productDetailsUnitCount: item.quantity,
-            productDetailsUnitCost: item.price,
-            productDetailsItemNumber: item.sku,
-            productDetailsItemName: sanitizeText(item.name),
-            productDetailsItemDescription: sanitizeText(item.title)
-          }))
+          productDetails: order.line_items?.map(item => ({
+            productDetailsUnitCount: item?.quantity,
+            productDetailsUnitCost: item?.price,
+            productDetailsItemNumber: item?.sku,
+            productDetailsItemName: sanitizeText(item?.name),
+            productDetailsItemDescription: sanitizeText(item?.title)
+          })) || []
         },
         payment: {
           paymentType: 'CC',
@@ -67,13 +67,13 @@ function buildOrderXML(order) {
           paymentCardNumber: '',
           paymentCardExpiration: '',
           paymentCardCVS: '',
-          paymentCardZip: order.billing_address.zip,
-          paymentCardHolderName: `${order.billing_address.first_name} ${order.billing_address.last_name}`,
+          paymentCardZip: order.billing_address?.zip,
+          paymentCardHolderName: `${order.billing_address?.first_name || ''} ${order.billing_address?.last_name || ''}`.trim(),
           paymentCardApproval: '',
           paymentCardTransID: '',
-          discountValue: order.total_discounts || '0.00',
+          discountValue: order?.total_discounts || '0.00',
           discountCode: order.discount_codes?.[0]?.code || '',
-          paymentTotal: order.total_price
+          paymentTotal: order?.total_price
         }
       }
     }
@@ -84,9 +84,9 @@ function buildOrderXML(order) {
 }
 
 function getCustomAttr(order, key) {
-  const attrs = [...(order.note_attributes || []), ...(order.line_items[0]?.properties || [])];
-  const attr = attrs.find((a) => a.name === key);
-  return attr ? sanitizeText(attr.value) : '';
+  const attrs = [...(order?.note_attributes || []), ...(order.line_items?.[0]?.properties || [])];
+  const attr = attrs.find((a) => a?.name === key);
+  return attr ? sanitizeText(attr?.value) : '';
 }
 
 module.exports = { buildOrderXML };
